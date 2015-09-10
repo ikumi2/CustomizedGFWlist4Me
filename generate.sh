cf35c21fead1736596824dd3464aa5d0774f5f1c
d0zingcat <unclecattony@gmail.com>
2015-09-10 17:42:36 +0800

    Updates

--- generate.sh
+++ generate.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+
+rm -rf gfwlist.txt
+rm -rf proxy.pac
+git add .
 #  Use local auto-bypass firewall config(Shadowsocks with port configured on 1080) to fetch gfwlist hosted on Github https://github.com/gfwlist/gfwlist
 curl -x SOCKS5://127.0.0.1:1080 https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt -o gfwlist.txt
-
 
 #pushd .. && \ Used for testing subfolder switchy
 PYTHONPATH=. python gfwlist2pac/main.py  -i gfwlist.txt -f proxy.pac -p 'SOCKS5 127.0.0.1:1080'  --user-rule user-rule.txt --precise&& \
@@ -11,6 +14,7 @@ PYTHONPATH=. python gfwlist2pac/main.py  -i gfwlist.txt -f proxy.pac -p 'SOCKS5 
 #cat proxy.pac test_case.js > /tmp/test.js && \
 #node /tmp/test.js && \
 echo 'Generated Success!'
+
 git add .
 git commit -m "Updates"
 git push
